c76d911f6b9db8edeeeebbdd8edf55b4ccf082ae
anders elo <anders@kaerus.org>
2012-09-13 17:29:08 +0200

    use AMD format

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module) }
 
-module.exports = require( './lib/arango' );
+define(function (require) {
+	return require( './lib/arango' );
+});
